53ef92e953c4142f8507e72d41142a6496d4676c
aidanmontag13 <123411045+aidanmontag13@users.noreply.github.com>
2023-01-31 14:50:30 -0500

    Update motor_camera_test_green.py

--- motor_camera_test_green.py
+++ motor_camera_test_green.py
@@ -1,4 +1,4 @@
-#this code turns the motor when the the camera detects a person. The window to show what the camera sees does not appear when running the code.
+#the window to show what the camera sees does not appear when running the code.
 
 from __future__ import print_function
 import sys
@@ -7,6 +7,7 @@ import time
 from DFRobot_RaspberryPi_DC_Motor import DFRobot_DC_Motor_IIC as Board
 
 import cv2
+board = Board(1, 0x10) 
 
 #thres = 0.45 # Threshold to detect object
 
@@ -42,67 +43,6 @@ def getObjects(img, thres, nms, draw=True, objects=[]):
                     cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
                     cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
                     
-                   
-                    sys.path.append("../")
-
-
-
-                    board = Board(1, 0x10)    # Select bus 1, set address to 0x10
-
-                    def board_detect():
-                      l = board.detecte()
-                      print("Board list conform:")
-                      print(l)
-
-                    ''' print last operate status, users can use this variable to determine the result of a function call. '''
-                    def print_board_status():
-                      if board.last_operate_status == board.STA_OK:
-                        print("board status: everything ok")
-                      elif board.last_operate_status == board.STA_ERR:
-                        print("board status: unexpected error")
-                      elif board.last_operate_status == board.STA_ERR_DEVICE_NOT_DETECTED:
-                        print("board status: device not detected")
-                      elif board.last_operate_status == board.STA_ERR_PARAMETER:
-                        print("board status: parameter error, last operate no effective")
-                      elif board.last_operate_status == board.STA_ERR_SOFT_VERSION:
-                        print("board status: unsupport board framware version")
-
-                    if __name__ == "__main__":
-
-                      board_detect()    # If you forget address you had set, use this to detected them, must have class instance
-
-                      # Set board controler address, use it carefully, reboot module to make it effective
-                      '''
-                      board.set_addr(0x10)
-                      if board.last_operate_status != board.STA_OK:
-                        print("set board address faild")
-                      else:
-                        print("set board address success")
-                      '''
-
-                      while board.begin() != board.STA_OK:    # Board begin and check board status
-                        print_board_status()
-                        print("board begin faild")
-                        time.sleep(2)
-                      print("board begin success")
-
-                      # board.set_encoder_enable(board.ALL)                 # Set selected DC motor encoder enable
-                      board.set_encoder_disable(board.ALL)                  # Set selected DC motor encoder disable
-                      board.set_encoder_reduction_ratio(board.ALL, 43)      # Set selected DC motor encoder reduction ratio, test motor reduction ratio is 43.8
-
-                      board.set_moter_pwm_frequency(1000)   # Set DC motor pwm frequency to 1000HZ
-                      x=0 
-                      board.motor_movement([1], board.CCW, 50)    # DC motor 1 movement, orientation clockwise
-                      time.sleep(4)
-                      board.motor_stop(board.ALL)   #stop all DC motor
-                      print_board_status()
-                      time.sleep(4)
-                      x+=1 
-                      print(x)
-                      board.motor_stop(board.ALL)
-
-
-
     return img,objectInfo
 
 
@@ -117,6 +57,14 @@ if __name__ == "__main__":
     while True:
         success, img = cap.read()
         result, objectInfo = getObjects(img,0.45,0.2, objects=['person'])
-        print(objectInfo)
         #cv2.imshow("Output",img)
-        cv2.waitKey(1)
+        print(objectInfo)
+        if objectInfo != []:
+            print(objectInfo)
+            board.set_encoder_disable(board.ALL)                  # Set selected DC motor encoder disable
+            board.set_encoder_reduction_ratio(board.ALL, 43)      # Set selected DC motor encoder reduction ratio, test motor reduction ratio is 43.8
+            board.set_moter_pwm_frequency(1000)   # Set DC motor pwm frequency to 1000HZ
+            board.motor_movement([1,2], board.CCW, 50)
+            time.sleep(1)    # DC motor 1 movement, orientation clockwise
+        board.motor_stop(board.ALL) #stop all DC motor     
+       
