a5b5520869521b76d8e238e8ab6f62040ffa6f91
Gian-Mateo <64568308+AntiPersnlMyne@users.noreply.github.com>
2023-02-23 15:24:17 -0500

    Update Motor_Run_noVision.py
    Key inputs now work. Turns suffer but may work better without cables. Must press enter to make keys work, looking for solution.

--- Motor_Run_noVision.py
+++ Motor_Run_noVision.py
@@ -7,7 +7,6 @@ import time
 from DFRobot_RaspberryPi_DC_Motor import DFRobot_DC_Motor_IIC as Board
 import signal
 import atexit
-import msvcrt
 # IMPORTANT
 # Requires the library "DFRobot_RaspberryPi_DC_Motor" to be in the current working directory
 # That means in the same folder, or remove the commented sys.path for an adjustment
@@ -57,29 +56,33 @@ def go_back():
   time.sleep(0.5)
 def turn_left():  
   board.motor_movement([1], board.CW, 50)
-  board.motor_movement([2], board.CCW, 00)
+  board.motor_movement([2], board.CCW, 10)
   time.sleep(0.5)  
 def turn_right():
   board.motor_movement([2], board.CW, 50)
-  board.motor_movement([1], board.CCW, 00)
+  board.motor_movement([1], board.CCW, 10)
   time.sleep(0.5)
 
 def rc_controls():
   key = input('press key')
   if key == 'a':
     print("a is pressed")
-  elif key == 'd':
-    print("d is pressed")
-  elif key == 'w':
-    print("w is pressed")
+    turn_left()
   elif key == 's':
     print("s is pressed")
+    go_back()
+  elif key == 'w':
+    print("w is pressed")
+    go_straight()
+  elif key == 'd':
+    print("d is pressed")
+    turn_right()
   elif key == 'q':
     print("q is pressed")
     board.motor_stop(board.all)
     print_board_status()
     
-    
+
 # @@@@@ EXECUTION @@@@@@
 if __name__ == "__main__":
   # Register signals - Behavior as the code exits
